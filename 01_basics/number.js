72822619a67597349e9da4cb1dbd12f74f9cf830
ankit0511 <99800154+ankit0511@users.noreply.github.com>
2024-01-19 07:11:02 +0000

    number

---
+++ 01_basics/number.js
@@ -0,0 +1,12 @@
+let number =40
+console.log(number);
+let new_num = new Number(100)
+console.log(new_num);
+console.log(typeof(new_num));
+let des = 123.832
+console.log(des.toPrecision(4));
+console.log(des.toPrecision(2));
+// there are also so many operations and functions in this segments.. 
+/*
+such as MAX value MIN value tosString and so on .. 
+*/
